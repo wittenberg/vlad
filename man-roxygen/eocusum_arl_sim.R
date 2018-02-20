4fef77653b8588866c4bb9564e70283ecddbf3ba
Philipp Wittenberg <pwitten@hsu-hh.de>
2018-02-20 16:24:55 +0100

    Extend example.

--- man-roxygen/eocusum_arl_sim.R
+++ man-roxygen/eocusum_arl_sim.R
@@ -5,40 +5,40 @@
 #'
 #' @examples
 #' \dontrun{
-# require("vlad")
-# # Set seed for reproducibility
-# RNGkind("L'Ecuyer-CMRG")
-# set.seed(1234)
-# parallel::mc.reset.stream()
-# # Datasets
-# data("cardiacsurgery")
-# df1 <- subset(cardiacsurgery, select=c(Parsonnet, status))
-#
-# # Estimate coefficients from logit model
-# coeff1 <- round(coef(glm(status~Parsonnet, data=df1, family="binomial")), 3)
-#
-# # Number of cores
-#
-# nc <- parallel::detectCores()
-#
-# # Number of simulation runs
-# m <- 10^3
-# # Deterioration:
-# kopt <- optimal_k(QA=2, parsonnetscores=df1$Parsonnet, coeff=coeff1)
-# # 1. Determine critical value for given ARL
-# hdet <- eocusum_arl_h_sim(L0=370, df=df1, k=kopt, coeff=coeff1, m=m, nc=nc, side="low")
-# 2. Determine ARL and Standard Error
-# RLS <- do.call(c, parallel::mclapply(1:m, eocusum_arl_sim, h=hdet, k=kopt, df=df1, side="low",
-#                                      coeff=coeff1, mc.cores=nc))
-# data.frame(cbind("ARL"=mean(RLS), "ARLSE"=sd(RLS)/sqrt(m)))
-#
-# # Improvement:
-# kopt <- optimal_k(QA=1/2, parsonnetscores=df1$Parsonnet, coeff=coeff1)
-# # 1. Determine critical value for given ARL
-# hup <- eocusum_arl_h_sim(L0=370, df=df1, k=kopt, coeff=coeff1, m=m, nc=nc, side="up")
-# #
-# # 2. Determine ARL and Standard Error
-# RLS <- do.call(c, parallel::mclapply(1:m, eocusum_arl_sim, h=hup, k=kopt, df=df1, side="up",
-#                                      coeff=coeff1, mc.cores=nc))
-# data.frame(cbind("ARL"=mean(RLS), "ARLSE"=sd(RLS)/sqrt(m)))
+#' require("vlad")
+#' # Set seed for reproducibility
+#' RNGkind("L'Ecuyer-CMRG")
+#' set.seed(1234)
+#' parallel::mc.reset.stream()
+#' # Datasets
+#' data("cardiacsurgery")
+#' df1 <- subset(cardiacsurgery, select=c(Parsonnet, status))
+#'
+#' # Estimate coefficients from logit model
+#' coeff1 <- round(coef(glm(status~Parsonnet, data=df1, family="binomial")), 3)
+#'
+#' # Number of cores
+#'
+#' nc <- parallel::detectCores()
+#'
+#' # Number of simulation runs
+#' m <- 10^3
+#' # Deterioration:
+#' kopt <- optimal_k(QA=2, parsonnetscores=df1$Parsonnet, coeff=coeff1)
+#' # 1. Determine critical value for given ARL
+#' hdet <- eocusum_arl_h_sim(L0=370, df=df1, k=kopt, coeff=coeff1, m=m, nc=nc, side="low")
+#' 2. Determine ARL and Standard Error
+#' RLS <- do.call(c, parallel::mclapply(1:m, eocusum_arl_sim, h=hdet, k=kopt, df=df1, side="low",
+#'                                      coeff=coeff1, mc.cores=nc))
+#' data.frame(cbind("ARL"=mean(RLS), "ARLSE"=sd(RLS)/sqrt(m)))
+#'
+#' # Improvement:
+#' kopt <- optimal_k(QA=1/2, parsonnetscores=df1$Parsonnet, coeff=coeff1)
+#' # 1. Determine critical value for given ARL
+#' hup <- eocusum_arl_h_sim(L0=370, df=df1, k=kopt, coeff=coeff1, m=m, nc=nc, side="up")
+#' #
+#' # 2. Determine ARL and Standard Error
+#' RLS <- do.call(c, parallel::mclapply(1:m, eocusum_arl_sim, h=hup, k=kopt, df=df1, side="up",
+#'                                      coeff=coeff1, mc.cores=nc))
+#' data.frame(cbind("ARL"=mean(RLS), "ARLSE"=sd(RLS)/sqrt(m)))
 #' }
