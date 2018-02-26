c24a6b89b28d4575db0593963f033480e18ef22c
Philipp Wittenberg <pwitten@hsu-hh.de>
2018-02-26 14:51:39 +0100

    Edit Description.

--- R/package.R
+++ R/package.R
@@ -3,7 +3,7 @@
 #' Contains functions to set up risk-adjusted quality control charts
 #' in health care. For the variable life adjusted display (VLAD) and the
 #' risk-adjusted Cumulative Sum chart based on log-likelihood ratio statistic
-#' Average Run Lengths or control limits can be computed.
+#' Average Run Lengths and control limits can be computed.
 #' @docType package
 #' @author Philipp Wittenberg and Sven Knoth
 #' @importFrom Rcpp evalCpp
