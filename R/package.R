03402d33bb67c1d5fc604cfae3c894e577fe758f
Philipp Wittenberg <pwitten@hsu-hh.de>
2018-02-26 14:05:53 +0100

    Edit package description.

--- R/package.R
+++ R/package.R
@@ -1,8 +1,9 @@
 #' Variable Life Adjusted Display
 #'
-#' Contains functions to set up risk-adjusted quality control charts in health care. For the
-#' variable life adjusted display (VLAD) and RA-CUSUM based on log-likelihood ratio statistic parameters
-#' such as the Average Run Length (ARL) or control limits can be computed.
+#' Contains functions to set up risk-adjusted quality control charts
+#' in health care. For the variable life adjusted display (VLAD) and the
+#' risk-adjusted Cumulative Sum chart based on log-likelihood ratio statistic
+#' Average Run Lengths or control limits can be computed.
 #' @docType package
 #' @author Philipp Wittenberg and Sven Knoth
 #' @importFrom Rcpp evalCpp
