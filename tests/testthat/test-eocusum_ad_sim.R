7a88fa970e5501ce9003c620cbdf5646347bf06e
Philipp Wittenberg <pwitten@hsu-hh.de>
2020-07-23 21:57:43 +0200

    tests.

--- tests/testthat/test-eocusum_ad_sim.R
+++ tests/testthat/test-eocusum_ad_sim.R
@@ -51,3 +51,48 @@ h <- 1
 #   sidetest <- "A"
 #   expect_error(eocusum_ad_sim(r, k, h, df1, coeff1, coeff2, side = sidetest), fixed = TRUE)
 # })
+
+test_that("Iterative search procedure I", {
+  skip_on_cran()
+  skip_if(SKIP==TRUE, "skip this test now")
+
+  data("cardiacsurgery", package = "spcadjust")
+  SALLI <- cardiacsurgery %>% mutate(s = Parsonnet) %>%
+    mutate(y = ifelse(status == 1 & time <= 30, 1, 0),
+        phase = factor(ifelse(date < 2*365, "I", "II"))) %>%
+    filter(phase == "I") %>% select(s, y)
+
+  ## estimate risk model, get relative frequences and probabilities
+  mod1 <- glm(y ~ s, data = SALLI, family = "binomial")
+  y <- SALLI$y
+  pi1 <- fitted.values(mod1)
+
+  ## set up patient mix (risk model)
+  pmix <- data.frame(y, pi1, pi1)
+  h <- 2
+  m <- 1e3
+
+  set.seed(1234)
+  RQ <- 1
+  expected_results <- 1000
+  m <- 1e4
+  tol <- 0.3
+
+  # type = cond
+  kopt_det <- optimal_k(pmix, RA=2)
+  RLS <- sapply(1:m, eocusum_ad_sim, pmix, k=kopt_det, h=2, RQ = RQ, type = "cond", side = "low")
+  expect_equal(mean(RLS), 200, tolerance=tol)
+
+  kopt_imp <- optimal_k(pmix, RA=1/2)
+  RLS <- sapply(1:m, eocusum_ad_sim, pmix, k=kopt_imp, h=2, RQ = RQ, type = "cond", side = "up")
+  expect_equal(mean(RLS), 265, tolerance=tol)
+
+  # type = cycl
+  kopt_det <- optimal_k(pmix, RA=2)
+  RLS <- sapply(1:m, eocusum_ad_sim, pmix, k=kopt_det, h=2, RQ = RQ, type = "cycl", side = "low")
+  expect_equal(mean(RLS), 197, tolerance=tol)
+
+  kopt_imp <- optimal_k(pmix, RA=1/2)
+  RLS <- sapply(1:m, eocusum_ad_sim, pmix, k=kopt_imp, h=2, RQ = RQ, type = "cycl", side = "up")
+  expect_equal(mean(RLS), 160, tolerance=tol)
+})
--- tests/testthat/test-eocusum_arl_sim.R
+++ tests/testthat/test-eocusum_arl_sim.R
@@ -46,3 +46,64 @@ test_that("Input value for side", {
   sidetest <- "A"
   expect_error(eocusum_arl_sim(r, k, h, df1, coeff1, side = sidetest), fixed=TRUE)
 })
+
+test_that("Iterative search procedure I", {
+  skip_on_cran()
+  skip_if(SKIP==TRUE, "skip this test now")
+
+  data("cardiacsurgery", package = "spcadjust")
+  SALLI <- cardiacsurgery %>% mutate(s = Parsonnet) %>%
+    mutate(y = ifelse(status == 1 & time <= 30, 1, 0),
+        phase = factor(ifelse(date < 2*365, "I", "II"))) %>%
+    filter(phase == "I") %>% select(s, y)
+
+  ## estimate risk model, get relative frequences and probabilities
+  mod1 <- glm(y ~ s, data = SALLI, family = "binomial")
+  y <- SALLI$y
+  pi1 <- fitted.values(mod1)
+
+  ## set up patient mix (risk model)
+  pmix <- data.frame(y, pi1, pi1)
+  h <- 2
+  m <- 1e3
+
+  set.seed(1234)
+  RQ <- 1
+  expected_results <- 1000
+  m <- 1e4
+  tol <- 0.3
+
+  # yemp = FALSE
+  kopt_det <- optimal_k(pmix, RA=2)
+  RLS <- sapply(1:m, eocusum_arl_sim, pmix, k=kopt_det, h=2, RQ = RQ, yemp = FALSE, side = "low")
+  expect_equal(mean(RLS), 210, tolerance=tol)
+
+  kopt_imp <- optimal_k(pmix, RA=1/2)
+  RLS <- sapply(1:m, eocusum_arl_sim, pmix, k=kopt_imp, h=2, RQ = RQ, yemp = FALSE, side = "up")
+  expect_equal(mean(RLS), 304, tolerance=tol)
+
+  # yemp = TRUE
+  kopt_det <- optimal_k(pmix, RA=2)
+  RLS <- sapply(1:m, eocusum_arl_sim, pmix, k=kopt_det, h=2, RQ = RQ, yemp = TRUE, side = "low")
+  expect_equal(mean(RLS), 197, tolerance=tol)
+
+  kopt_imp <- optimal_k(pmix, RA=1/2)
+  RLS <- sapply(1:m, eocusum_arl_sim, pmix, k=kopt_imp, h=2, RQ = RQ, yemp = TRUE, side = "up")
+  expect_equal(mean(RLS), 286, tolerance=tol)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
