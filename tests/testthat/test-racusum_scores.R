1003083e9b67d7b6ac66763c71578c402b34454f
Philipp Wittenberg <pwitten@hsu-hh.de>
2020-08-06 08:58:49 +0200

    testing.

--- tests/testthat/test-racusum_scores.R
+++ tests/testthat/test-racusum_scores.R
@@ -11,6 +11,6 @@ test_that("patient Cusum values with different odds ratios, see Rigdon and Frick
   expected_results <- list(s1=0, s1l=0.05083)
   wt1 <- round(llr_score(df = data.frame(19L, 0), coeff = coeff, R0 = 1, RA = 2), 4)
   wt2 <- round(llr_score(df = data.frame(19L, 0), coeff = coeff, R0 = 1, RA = 1/2), 5)
-  works <- racusum_scores(wt1 = wt1, wt2 = wt2, reset = TRUE, h1 = 4.5, h2 = 4)
+  works <- racusum_scores(wt1 = wt1, wt2 = wt2, reset = TRUE, h1 = .1, h2 = .1)
   expect_equal(works, expected_results)
 })
--- tests/testthat/test-search_delta.R
+++ tests/testthat/test-search_delta.R
@@ -8,17 +8,18 @@ SALL <- cardiacsurgery %>% rename(s = Parsonnet) %>%
   mutate(y = ifelse(status == 1 & time <= 30, 1, 0),
          phase = factor(ifelse(date < 2*365, "I", "II")))
 
+tol <- 1e-6
 ## subset phase I (In-control)
 SI <- filter(SALL, phase == "I") %>% select(s, y)
 
 test_that("Search delta: ML", {
   expected_results <- .06304707
   works <- search_delta(SI$s, SI$y, type = "ML")
-  expect_equal(works, expected_results)
+  expect_equal(works, expected_results, tolerance=tol)
 })
 
 test_that("Search delta: Pearson", {
   expected_results <- .07158091
   works <- search_delta(SI$s, SI$y, type = "Pearson")
-  expect_equal(works, expected_results)
+  expect_equal(works, expected_results, tolerance=tol)
 })
